347fb90c7b9df0ba949b4bc926f03fea4e5349d1
dpslwk <dps.lwk@gmail.com>
2024-08-02 23:26:09 +0100

    tables for Humidity, Pressure and Battery

---
+++ schema/tb_barometric_pressures.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `barometric_pressures`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `barometric_pressures` (
+  `sensor` varchar(30) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `reading` double DEFAULT NULL,
+  `time` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`sensor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
